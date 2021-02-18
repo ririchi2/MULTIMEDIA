ed29b6593a8a566fecd1214742d38e318708aef5
Ricardo Pineda <richipineda2@gmail.com>
2021-02-18 22:18:24 +0000

    .

---
+++ js/actions.js
@@ -0,0 +1,33 @@
+
+
+
+var slowBtn = document.getElementById("slowBtn");
+var normalBtn = document.getElementById("normalBtn");
+var fastBtn = document.getElementById("fastBtn");
+var video = document.getElementById("videotag");
+
+
+slowBtn.addEventListener('click' ,() => {    
+    video.playbackRate= 0.5;
+    if (video.paused) 
+    video.play(); 
+    else 
+    video.pause();
+});
+
+normalBtn.addEventListener('click' ,() => {
+    video.playbackRate = 1;
+    if (video.paused) 
+    video.play(); 
+    else 
+    video.pause(); 
+    
+})
+
+fastBtn.addEventListener('click',() =>{
+    video.playbackRate= 2;
+    if (video.paused) 
+    video.play(); 
+    else 
+    video.pause();
+} )
